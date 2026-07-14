4485a8f4f7e8212b68c55f092e8069134c091f30
Subharjun28 <156801384+Subharjun@users.noreply.github.com>
2026-07-14 18:00:49 +0530

    refactor: migrate src/index.js for chalk 5.6.2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const chalk = require('chalk');
-const express = require('express');
+import chalk from 'chalk';
+import express from 'express';
 const app = express();
 app.get('/', (req, res) => res.send(chalk.green('hello from a very outdated stack')));
 app.listen(3000, () => console.log(chalk.blue('listening on :3000')));
